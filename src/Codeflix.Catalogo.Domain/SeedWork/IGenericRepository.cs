b3474aa9c77e4d2307f4030f8b66a3d8c6ca8599
Guilherme Lazani Lopes <guilhermelazarinilopes@gmail.com>
2024-11-18 22:47:49 -0300

    Add Generic Repo

--- src/Codeflix.Catalogo.Domain/SeedWork/IGenericRepository.cs
+++ src/Codeflix.Catalogo.Domain/SeedWork/IGenericRepository.cs
@@ -4,5 +4,5 @@ using Codeflix.Catalogo.Domain.Entity;
 namespace Codeflix.Catalogo.Domain.SeedWork;
 public interface IGenericRepository<TAggregate> : IRepository
 {
-    public Task Add(TAggregate aggregat, CancellationToken cancellationToken);
+    public Task Insert(TAggregate aggregat, CancellationToken cancellationToken);
 }
